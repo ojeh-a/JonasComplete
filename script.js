3d6a53b37baa2d0a29e14003f64ab0f360bb297a
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-02-21 12:46:08 +0100

    innoliz lecture

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ if (scoreDolphins > scoreKoalas) {
 } else {
   console.log("Both team wins");
 }
+const bill = 430;
+
+let tip;
+bill > 50 && bill <= 300 ? (tip = bill * 0.12) : (tip = bill * 0.2);
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+);
