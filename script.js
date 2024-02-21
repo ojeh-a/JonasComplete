7abbaeb3ff4fc3ed40a84429a7c5e26c34e1b575
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-02-21 10:01:19 +0100

    innoliz lecture

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-const favourite = Number(prompt("enter a number"));
-console.log(`your fav number is ${favourite}`);
-console.log(typeof favourite);
+const dolphins = 96 + 108 + 89;
+const koalas = 88 + 91 + 110;
+const scoreDolphins = dolphins / 3;
+const scoreKoalas = koalas / 3;
+console.log(scoreKoalas, scoreDolphins);
+
+if (scoreDolphins > scoreKoalas) {
+  console.log("Dolphins win the trophy");
+} else if (scoreKoalas > scoreDolphins) {
+  console.log("koalas wins the trophy");
+} else {
+  console.log("Both team wins");
+}
