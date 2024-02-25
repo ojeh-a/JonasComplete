2d550f2c3640593b8c62cf5fb379142276fb4c12
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-02-25 04:59:38 +0100

    innoliz class

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const dolphins = 96 + 108 + 89;
 const koalas = 88 + 91 + 110;
 const scoreDolphins = dolphins / 3;
@@ -22,3 +24,15 @@ const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 console.log(
   `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
 );
+function makeJuice(a, b) {
+  const juice = `hello ${a} and ${b}`;
+  return juice;
+}
+console.log(makeJuice(14, 30));
+console.log(makeJuice(0, 9));
+console.log("hello world");
+function calAge(birthyear) {
+  return 2024 - birthyear;
+}
+const hello = calAge(1987);
+console.log(hello);
