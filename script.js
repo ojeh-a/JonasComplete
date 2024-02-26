9f5b34274406862b35a538e6124d56c821479919
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-02-26 15:09:04 +0100

    innoliz function

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 "use strict";
 
-const dolphins = 96 + 108 + 89;
-const koalas = 88 + 91 + 110;
-const scoreDolphins = dolphins / 3;
-const scoreKoalas = koalas / 3;
-console.log(scoreKoalas, scoreDolphins);
+// const dolphins = 96 + 108 + 89;
+// const koalas = 88 + 91 + 110;
+// const scoreDolphins = dolphins / 3;
+// const scoreKoalas = koalas / 3;
+// console.log(scoreKoalas, scoreDolphins);
 
-if (scoreDolphins > scoreKoalas) {
-  console.log("Dolphins win the trophy");
-} else if (scoreKoalas > scoreDolphins) {
-  console.log("koalas wins the trophy");
-} else {
-  console.log("Both team wins");
-}
-const bill = 1400;
+// if (scoreDolphins > scoreKoalas) {
+//   console.log("Dolphins win the trophy");
+// } else if (scoreKoalas > scoreDolphins) {
+//   console.log("koalas wins the trophy");
+// } else {
+//   console.log("Both team wins");
+// }
+// const bill = 1400;
 
 // let tip;
 // bill >= 50 && bill <= 300 ? (tip = bill * 0.15) : (tip = bill * 0.2);
 // console.log(
 //   `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
 // );
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-console.log(
-  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
-);
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// console.log(
+//   `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+// );
 function makeJuice(a, b) {
   const juice = `hello ${a} and ${b}`;
   return juice;
@@ -57,25 +57,32 @@ function makeJuice(apple, orange) {
 }
 console.log(makeJuice(20, 10));
 /* Write your code below. Good luck! 🙂 */
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+// let scoreDolphins = calcAverage(40, 57, 56);
+// let scoreKoalas = calcAverage(40, 57, 56);
 
-const calcAverage = (a, b, c) => {
-  const averageScore = (a + b + c) / 3;
-  return averageScore;
-};
-let scoreDolphin = calAverage(44, 23, 71);
-let scoreKoala = calAverage(65, 54, 49);
-console.log(scoreDolphin);
+// const checkWinner = (avgDolphins, avgKoalas) => {
+//   if (avgDolphins >= avgKoalas * 2) {
+//     console.log("heollllll");
+//   } else if (avgKoalas >= avgDolphins * 2) {
+//     console.log("hsudjhj");
+//   }
+// };
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
 
-const checkWinner = (scoreDolphin, scoreKoala) => {
-  if (scoreDolphin > scoreKoala && scoreDolphin >= (scoreKoala ^ 2)) {
-    return console.log(`Dolphins win (${scoreDolphin} vs. ${scoreKoala}`);
-  } else if (
-    scoreKoala > scoreDolphin &&
-    scoreKoala >= scoreDolphin * scoreDolphin
-  ) {
-    return console.log(`Koalas win (${scoreKoala} vs. ${scoreDolphin}`);
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
   } else {
-    return `no team wins`;
+    console.log("No team wins...");
   }
 };
-checkWinner(scoreDolphin, scoreKoala);
+
+checkWinner(scoreDolphins, scoreKoalas);
