ba0c7c31555d95e650984e1e3636599f74aad697
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-03-05 12:10:29 +0100

    clearing innoliz

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
 "use strict";
 
-// const dolphins = 96 + 108 + 89;
-// const koalas = 88 + 91 + 110;
-// const scoreDolphins = dolphins / 3;
-// const scoreKoalas = koalas / 3;
-// console.log(scoreKoalas, scoreDolphins);
-
-// if (scoreDolphins > scoreKoalas) {
-//   console.log("Dolphins win the trophy");
-// } else if (scoreKoalas > scoreDolphins) {
-//   console.log("koalas wins the trophy");
-// } else {
-//   console.log("Both team wins");
-// }
-// const bill = 1400;
-
-// let tip;
-// bill >= 50 && bill <= 300 ? (tip = bill * 0.15) : (tip = bill * 0.2);
-// console.log(
-//   `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
-// );
-// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-// console.log(
-//   `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
-// );
 function makeJuice(a, b) {
   const juice = `hello ${a} and ${b}`;
   return juice;
