65e550897a4e90be659838f91b993a2e5cd8b1f6
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-07-10 11:20:52 +0100

    dom

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ const head1 = document.querySelector(".header");
 console.log(head1);
 console.log("hello");
 head1.style.color = "red";
+
+const allItems = document.querySelectorAll(".list-item");
+for (let i = 0; i < allItems.length; i++) {
+  allItems[i].style.fontSize = "0.8rem";
+  allItems[i].style.color = "powderblue";
+  allItems[i].style.backgroundColor = "white";
+}
