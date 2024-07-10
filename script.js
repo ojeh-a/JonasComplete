9ff95c88e996478ac43fa8c0beea1c417f2b2541
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-07-10 10:45:36 +0100

    dom

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 document.getElementById("btn").addEventListener("click", () => {
   document.querySelector(".text").style.backgroundColor = "red";
 });
+const head1 = document.querySelector(".header");
+console.log(head1);
+console.log("hello");
+head1.style.color = "red";
