0b27dace9ac4d28d38deda4a554796762b2324c4
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-08-08 05:59:32 +0100

    if else class

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+let age = 12;
+if (age >= 18) {
+  console.log("Tope is old enough to drive");
+} else {
+  const underAge = 18 - age;
+  console.log(`
+    Ouch!
+    Too young. 
+    Wait for next ${underAge} years`);
+}
