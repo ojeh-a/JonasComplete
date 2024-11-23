b11220c2e31fcffb0cc6dab6aa23e8a59f70a5a5
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-11-23 12:01:10 +0100

    hta academy class

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 "use strict";
 const check = document.querySelector(".hello");
 console.log(check);
+const maker = (sand, water) => {
+  const man = `Man made with ${sand} cups of sand and ${water} liters of water`;
+  return man;
+};
+console.log(maker(20, 15));
