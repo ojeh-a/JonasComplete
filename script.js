6287083fd15f9886b72d9be6b2c25ca256d85613
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-12-12 20:53:51 +0100

    hta academy class

--- script.js
+++ script.js
@@ -40,24 +40,75 @@ if (age > 18) {
 console.log(age);
 
 let currentAge = 0;
-// if (currentAge <= 14 || currentAge >= 65) {
-//   console.log("drink water");
-// } else if (currentAge > 14 && currentAge < 22) {
-//   console.log("drink juice");
-// } else if (currentAge > 21 && currentAge < 46) {
-//   console.log("Drink alcohol");
-// } else {
-//   console.log("we no send you");
-// }
-currentAge <= 14 || currentAge >= 65
-  ? console.log("drink water")
-  : currentAge > 14 && currentAge < 22
-  ? console.log("drink juice")
-  : console.log("hello");
+if (currentAge <= 14 || currentAge >= 65) {
+  console.log("drink water");
+} else if (currentAge > 14 && currentAge < 22) {
+  console.log("drink juice");
+} else if (currentAge > 21 && currentAge < 46) {
+  console.log("Drink alcohol");
+} else {
+  console.log("we no send you");
+}
+console.log(Math.floor(Math.random() * 29) + 1);
 
-church = "cms";
-church === "rccg"
-  ? console.log("She is from redeem")
-  : church === "cac"
-  ? console.log("she is from cac")
-  : console.log("baga lizard");
+let currAge = 100;
+if (currAge <= 18) {
+  console.log("drink water 💧");
+} else if (currAge >= 22 && currAge <= 35) {
+  console.log("Smoke weed 🌴");
+} else if (currAge >= 36 && currAge <= 50) {
+  console.log("eat bread and beans");
+} else if (currAge >= 51 && currAge <= 90) {
+  console.log("go and sleep");
+} else {
+  console.log("hsck wine");
+}
+
+currAge <= 18
+  ? console.log("drink water 💧💧💧💧")
+  : currAge >= 22 && currAge <= 35
+  ? console.log("Smoke weed 🌴🌴🌴🌴")
+  : console.log("shack wine🍷🍷🍷");
+
+let state = "ogun";
+if (state === "lagos") {
+  console.log("Eko ile ogbon");
+} else if (state === "oyo") {
+  console.log("amala people");
+} else if (state === "enugu") {
+  console.log("coal city");
+} else {
+  console.log("gettat");
+}
+
+state === "lagos"
+  ? console.log("Eko ile Ogbon")
+  : state === "oyo"
+  ? console.log("Largest city dey here")
+  : state === "enugu"
+  ? console.log("Coal city")
+  : console.log("gettat");
+
+let isGood = true;
+let isBlind = false;
+let isDumb = false;
+let agePerson = 30;
+
+if (isBlind && agePerson >= 30) {
+  console.log("hello");
+} else if (isBlind && isDumb) {
+  console.log("hhiiiiiiiiiiii");
+} else if (isDumb && isGood) {
+  console.log("ooooooooooo");
+} else {
+  console.log("not found");
+}
+
+let hiddenNumber = 2;
+if (hiddenNumber < 15) {
+  console.log("guess is too low ");
+} else if (hiddenNumber === 15) {
+  console.log("You don get am ");
+} else if (hiddenNumber > 15) {
+  console.log("your guess is too high");
+}
